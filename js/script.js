04d7e2714815d1628d5f6c0adcf4b20a455b10aa
willie capre <wcap18@gmail.com>
2019-05-24 17:53:13 -0400

    added jquery
    added file

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ FSJS project 3 - Interaction Form
 
 //When the page loads, the first text field needs be in focus.
 
-document.getElementById("page").focus();
+//document.getElementById("page").focus();
 
 //Include a text field that will be revealed when the "Other"
 // option is selected from the "Job Role" drop down menu.
@@ -34,3 +34,25 @@ document.getElementById("other-title").addEventListener("change", function(){
 		}
 	}
 });
+
+//For the T-Shirt "Color" menu, only display the color options that match 
+//the design selected in the "Design" menu.
+
+document.getElementById("design").addEventListener("color", function () {
+var selectedDesign = designSelection.options[designSelection.selectedIndex].value;
+var jsPuns = document.getElementById("design").options[value="js puns"];
+var iHeartJs = document.getElementById("design").options[value="heart js"];
+
+//whichever design has been selected, match color will be displayed, does that doesn't macth will be hiding.
+if (selectedDesign === jsPuns) {
+    var showColors = document.querySelector("select#color option[value=cornflowerblue, darkslategrey, gold]");
+    showColors.style.display = 'block';
+    var hideColors = document.getElementsByName("heart");
+    hideColors.style.display = 'none';
+}
+
+});
+
+var $name = $("#name");
+var $mail = $("#mail");
+var $payment = $("#payment");
