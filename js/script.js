9b6b8acf7c1cf11454f6ea9da575b60450ba7516
willie capre <wcap18@gmail.com>
2019-05-28 19:31:27 -0400

    added files
    file

--- js/script.js
+++ js/script.js
@@ -164,7 +164,47 @@ $('#paymenyOptions').on('input', function() {
 }
     else{input.removeClass("valid").addClass("invalid");
 }
-
+var constraints = {
+    creditCardNumber: {
+      presence: true,
+      format: {
+        pattern: /^(34|37|4|5[1-5]).*$/,
+        message: function(value, attribute, validatorOptions, attributes, globalOptions) {
+          return validate.format("^%{num} is not a valid credit card number", {
+            num: value
+          });
+        }
+      },
+      length: function(value, attributes, attributeName, options, constraints) {
+        if (value) {
+          // Amex
+          if ((/^(34|37).*$/).test(value)) return {is: 15};
+          // Visa, Mastercard
+          if ((/^(4|5[1-5]).*$/).test(value)) return {is: 16};
+        }
+        // Unknown card, don't validate length
+        return false;
+      }
+    },
+    creditCardZip: function(value, attributes, attributeName, options, constraints) {
+      if (!(/^(34|37).*$/).test(attributes.creditCardNumber)) return null;
+      return {
+        presence: {message: "is required when using AMEX"},
+        length: {is: 5}
+      };
+    }
+  };
+  
+  validate({creditCardNumber: "4"}, constraints);
+  // => {"creditCardNumber": ["Credit card (should be 16 characters)"]}
+  
+  validate({creditCardNumber: "9999999999999999"}, constraints);
+  // => {"creditCardNumber": ["9999999999999999  not a valid credit card number"]}
+  
+  validate({creditCardNumber: "4242424242424242"}, constraints);
+  // => undefined
+  
+  validate({creditCardNumber: "340000000000000"}, constraints);
 
 
 // must enter a message, this can't leave blank
